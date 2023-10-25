8efe4bb88488caecb27a5e4d1a8f4309034b7410
chernyatinai <chernyatinai@www-shop-01.dev.erkapharm.ru>
2023-10-25 12:46:30 +0300

    created the Post entity

---
+++ database/migrations/2023_10_25_091923_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('id');
+            $table->integer('version');
+            $table->string('title');
+            $table->json('content');
+            $table->boolean('status')->default(false);
+            $table->boolean('noindex')->default(false);
+            $table->timestamp('last_modified_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
