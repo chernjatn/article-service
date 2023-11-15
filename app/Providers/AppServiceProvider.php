cf5cc67adf587207c0668c83cd2131a27dc9da23
khitrov.av <khitrov.av@erkapharm.com>
2023-11-15 18:31:14 +0300

    Revert "Изменение URL и схемы для боевой среды"
    This reverts commit 0b002eeeb41eed956d8adc119f3b5c080b9f7d0f.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')) {
-            \URL::forceRootUrl(config('app.url'));
-            \URL::forceScheme('https');
-        }
+        //
     }
 }
