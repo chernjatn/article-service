c93d0e2e007189ee6876672e658459d6656beb70
Чернятин Андрей Игоревич <chernyatin.ai@erkapharm.com>
2023-12-15 18:34:21 +0300

    revert seo relation

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -47,6 +47,11 @@ class Article extends Model implements HasMedia
         return $this->belongsTo(Author::class);
     }
 
+    public function seo(): BelongsTo
+    {
+        return $this->belongsTo(Seo::class);
+    }
+
     public function heading(): BelongsTo
     {
         return $this->belongsTo(Heading::class);
